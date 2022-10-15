524f19ebd3bc300257fb1f4d275c446e5e4a8cfa
Yassin <yassinpellicerlamla@gmail.com>
2022-10-15 21:23:34 +0200

    commit

---
+++ Processes/pract4/my_child.c
@@ -0,0 +1,11 @@
+/***code of my_child.c***/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    printf("Process %ld \n", (long)getpid());
+    fork();
+    printf("Process %ld,my parent %ld\n" ,(long)getpid(),(long)getppid());
+    sleep(15);
+    return 0;
+}
