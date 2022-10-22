42d17e6aa4e43cf5f87df7c5039aeee30051f37b
Yassin <yassinpellicerlamla@gmail.com>
2022-10-22 02:50:38 +0200

    yviuy

--- FSO/prac3/shell/ej13.sh
+++ FSO/prac3/shell/ej13.sh
@@ -1,3 +1,2 @@
-model=$(tr -d '\0' < /proc/device-tree/model)
 echo -e "PID\tPPID\tSTATE\tCOMMAND"
-echo -e "$(awk '{print $1,"\t"$4,"\t"$3"\t"}' /proc/$1/stat) $(awk '{print $1}' /proc/$1/cmdline)"
+echo -e $(awk '{printf $1,"\t"$4,"\t"$3"\t"}' /proc/$1/stat) $(awk '{printf $1}' /proc/$1/cmdline)
