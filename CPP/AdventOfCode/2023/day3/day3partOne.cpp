a27912c5b1e0c176288dd7d14ee817d4df42f07a
yassin <yassinpellicerlamla@gmail.com>
2023-12-15 22:10:51 +0100

    commit

--- CPP/AdventOfCode/2023/day3/day3partOne.cpp
+++ CPP/AdventOfCode/2023/day3/day3partOne.cpp
@@ -4,6 +4,13 @@
 #include <string>
 #include <fstream>
 
+bool isContained(std::string t, char a){
+    for(int i = 0; i < t.length(); i++){
+        if(t.at(i) == a){return true;}
+    }
+    return false;
+}
+
 std::vector<std::vector<char>>* convertToArray (std::string fileName){
     std::vector<std::vector<char>>* array = new std::vector<std::vector<char>>;
 
@@ -24,8 +31,49 @@ std::vector<std::vector<char>>* convertToArray (std::string fileName){
     return array;
 }
 
+bool checkNeighbours(std::vector<int>* y, std::vector<int>* x, std::vector<std::vector<char>>* array){
+    for(int i = 0; i < y->size(); i++){
+        for(int col = -1; col <= 1; col++){
+            for(int row = -1; row <= 1; row++){
+                if((y->at(col) != -1 && x->at(row) != -1) &&
+                   (y->at(col) != array->size() && x->at(row) != array->at(col).size())){
+                    if(!isContained("1234567890.", array->at(col).at(row))) return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int sumNumbers(std::vector<std::vector<char>>* array){
+    std::string numberString = "0123456789"; 
+    int sum = 0;
+    int j = 0;
+    std::vector<int>* y;
+    std::vector<int>* x;
+
+    for(int j = 0; j < array->size(); j++){
+        std::string numString = "";
+        for(int i = 0; i < array->at(j).size(); i++){
+            if(isContained(numberString, array->at(j).at(i))){
+                numString += array->at(j).at(i);
+                y->push_back(j);
+                x->push_back(i);
+            }
+            else if(numString != ""){
+                if(checkNeighbours(y, x, array)){sum += std::stoi(numString);}
+                numString = "";
+                y->clear();
+                x->clear();
+            }
+        }
+    }
+    return sum;
+}
+
 int main(){
     std::vector<std::vector<char>>* array = convertToArray("input.txt");
+    std::cout << sumNumbers(array) << std::endl;
     return 0;
 }
 
